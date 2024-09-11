dcac85a9ab04ba6e3a3d5438e5855390a1d39a48
Aniashi <anielcarvalho01@gmail.com>
2024-09-11 14:14:30 -0300

    Feat: Exercício 2

--- src/lista2.java
+++ src/lista2.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class lista2 {
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
+        double emprestimo;
+        double salario;
+        int parcelas;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Valor do empréstimo: ");
+        emprestimo = ler.nextDouble();
+        System.out.println("Quantidade de parcelas:");
+        parcelas = ler.nextInt();
+        System.out.println("Seu salário:");
+        salario = ler.nextDouble();
+        if(emprestimo <=30% salario){
+            System.out.println("Empréstimo reprovado");
+        }else{
+            System.out.println("Empréstimo aprovado");
+        }
         
     }
 }
